278aa20b2c3c16a4e358563f1775f5a5efce898f
Mrmagicpie <70033286+Mrmagicpie@users.noreply.github.com>
2021-10-09 23:42:21 -0400

    Create example_french.py

---
+++ French/example_french.py
@@ -0,0 +1,27 @@
+"""dsoifudhdjsma"""
+"""
+iuydghbsnkaopfdiiujklsdfoiu"""
+importer builtins 
+
+# eeee 
+#
+# lmao yea 
+#
+# eeee 
+
+"""
+
+no u
+"""
+
+pour e dans ["e"]:
+    print(e)
+    si ne Vrai pas:
+        ded("e")
+déf aa():
+    print("aa")
+
+si ne Faux:
+    aa()
+
+print(builtins)
